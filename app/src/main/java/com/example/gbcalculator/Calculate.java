d675ac959c3b10a84ba79547dbdc2ea5f31b89c3
yelzhan <11l.yelzhank@gmail.com>
2021-07-16 01:27:42 +0600

    Урок 3. Активити. Жизненный цикл (1,2)

---
+++ app/src/main/java/com/example/gbcalculator/Calculate.java
@@ -0,0 +1,72 @@
+package com.example.gbcalculator;
+
+public class Calculate {
+    private int number_1;
+    private int number_2;
+    private int number_result;
+    private String operator;
+
+    public Calculate() {
+        this(0, 0, "");
+    }
+
+    public Calculate(int number_1, int number_2, String operator) {
+        this.number_1 = number_1;
+        this.number_2 = number_2;
+        this.operator = operator;
+    }
+
+    public int getNumber_1() {
+        return number_1;
+    }
+
+    public int getNumber_2() {
+        return number_2;
+    }
+
+    public int getNumber_result() {
+        return number_result;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setNumber_1(int number_1) {
+        this.number_1 = number_1;
+    }
+
+    public void setNumber_2(int number_2) {
+        this.number_2 = number_2;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public int o_plus(int number_1, int number_2) {
+        number_result = number_1 + number_2;
+        return number_result;
+    }
+
+    public int o_minus(int number_1, int number_2) {
+        number_result = number_1 - number_2;
+        return number_result;
+    }
+
+    public int o_divide(int number_1, int number_2) {
+        if (number_2 == 0) {
+            number_result = 0;
+        } else {
+            number_result = number_1 / number_2;
+        }
+
+        return number_result;
+    }
+
+    public int o_multiply(int number_1, int number_2) {
+        number_result = number_1 * number_2;
+        return number_result;
+    }
+
+}
